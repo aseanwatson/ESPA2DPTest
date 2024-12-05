5d1d4e64975f41547fe527a3d196bff042d280ed
Sean <aseanwatson@outlook.com>
2024-12-05 00:14:13 -0800

    Basic "Hello World" functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,69 @@
 #include <Arduino.h>
+#include "AudioConfig.h"
+#include "BluetoothA2DPSource.h"
+//#include "esp_spiffs.h"
+#include "SPIFFS.h"
+#include <vector>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothA2DPSource a2dp_source;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+class FileOneChannelSoundData : public OneChannelSoundData
+{
+public:
+    FileOneChannelSoundData(bool loop = false, ChannelInfo channelInfo = Both)
+        : OneChannelSoundData(loop, channelInfo)
+    {
+        unload();
+    }
+
+    void load(fs::File& file)
+    {
+        // unload in case resize moves buffer
+        unload();
+
+        file.seek(0);
+
+        // make _data bigger (if needed) to accomidate file
+        _data.resize(file.size());
+
+        // read the data, capturing the actual number of bytes read
+        size_t len = file.read(_data.data(), _data.size());
+
+        // make sure the data have the size of data read
+        _data.resize(len);
+
+        // update the base class with the dynamic data
+        setDataRaw(_data.data(), len);
+    }
+
+    void unload()
+    {
+        setDataRaw(NULL, 0);
+    }
+
+private:
+    std::vector<uint8_t> _data;
+};
+
+FileOneChannelSoundData soundData(true);
+
+void loadSamples(const char* filename)
+{
+    fs::File file = SPIFFS.open(filename);
+    soundData.load(file);
+    file.close();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    SPIFFS.begin();
+    loadSamples("/hello.raw");
+
+    a2dp_source.start("Zone Wireless"); /* Zone Wireless == name of my headphones*/
+    a2dp_source.write_data(&soundData);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    delay(100);
 }
